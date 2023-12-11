b45f1bde677363af56fe4e26cce8874b507f643d
vsheva <valera.sheva@gmail.com>
2023-12-11 01:25:05 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "./user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+activatedButton = false
+  private activatedSubscription: Subscription
 
-  ngOnInit() {}
+  constructor(private userService:UserService) {}
+
+  ngOnInit() {
+    this.activatedSubscription= this.userService.activatedEmitter.subscribe((wasActivated) => {
+    this.activatedButton = wasActivated;
+  })
+  }
+
+  ngOnDestroy() {
+    this.activatedSubscription.unsubscribe()
+  }
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,8 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private userService:UserService) {
   }
 
   ngOnInit() {
@@ -17,4 +19,11 @@ export class UserComponent implements OnInit {
       this.id = +params.id;
     });
   }
+
+  onActivate() {
+    //this.userService.activatedEmitter.emit(true)
+    this.userService.activatedEmitter.next(true)
+  }
+
+
 }
