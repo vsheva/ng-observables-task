b404b51de79f34f7fe1340b7dfecf1c02dd8fbee
vsheva <valera.sheva@gmail.com>
2023-12-11 00:10:48 -0500

    operators map and filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {interval, Observable, Subscription} from "rxjs";
+import {map, filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -24,23 +25,27 @@ export class HomeComponent implements OnInit {
 
         setInterval(()=>{
           observer.next(count); //!!
-          // if(count===2) {
-          //   observer.complete();}
-
-          if(count>3) {
-            observer.error( new Error('Count is grater then 3!'))
-          }
-
           if(count===7) {
             observer.complete();
           }
 
+          if(count>3) {
+            observer.error( new Error('Count is grater then 3!'))
+          }
           count++
         },1000)
       }
     )
 
-    this.firstObservableSubs= customObservable.subscribe(
+   /* customObservable.pipe(map((data:number) =>{
+      return "Round: " + (data +1);
+    }))
+*/
+
+
+    this.firstObservableSubs=  customObservable.pipe(filter((data:number) =>{return data>0}),map((data:number) =>{
+      return "Round: " + (data +1);
+    })).subscribe(
       (data)=>console.log(data),
       (error)=>{
         alert(error.message)
